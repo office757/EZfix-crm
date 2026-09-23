d1c37309762d607c728abb772075ba557e299539
office757 <office@ezfixgaragedoorsinc.com>
2026-09-23 15:24:43 -0400

    Flag unverified technician-arrangement claims

--- supabase/functions/process-inkbox-call-leads/index.ts
+++ supabase/functions/process-inkbox-call-leads/index.ts
@@ -15,7 +15,7 @@ function guardrails(t:any,assignmentAllowed:boolean){
  const local=localTextOf(t), lines=localLines(t);
  const paymentAmountPrompt=/\b(payment amount|exact amount|dollar amount|amount (?:would|do|should) you (?:like|want|use)|how much (?:would|do|should) you (?:like|want) to pay)\b/i.test(local);
  const paymentSchedulingClaim=lines.some((line:string)=>/\b(?:schedule|arrange|set up|book)\b.{0,35}\b(?:payment|payment amount|dollar amount)\b|\b(?:payment|payment amount|dollar amount)\b.{0,35}\b(?:schedule|arrange|set up|book)\b/i.test(line));
- const definitiveBookingRe=/\b(?:your appointment (?:is|has been) (?:booked|scheduled|confirmed|reserved)|you(?:['’]re| are) (?:booked|scheduled|confirmed|reserved)|i(?:['’]ve| have) (?:booked|scheduled|confirmed|reserved)|i(?:['’]ll| will| can(?: help)?) (?:book|schedule|arrange|reserve)|your service (?:is|has been) (?:booked|scheduled|confirmed|reserved)|(?:technician|tech) (?:will|is going to) (?:arrive|be there))\b/i;
+ const definitiveBookingRe=/\b(?:your appointment (?:is|has been) (?:booked|scheduled|confirmed|reserved)|you(?:['’]re| are) (?:booked|scheduled|confirmed|reserved)|i(?:['’]ve| have) (?:booked|scheduled|confirmed|reserved)|(?:i|we)(?:['’]ll| will| can(?: help)?) (?:book|schedule|arrange|reserve)|your service (?:is|has been) (?:booked|scheduled|confirmed|reserved)|(?:technician|tech) (?:will|is going to) (?:arrive|be there))\b/i;
  const qualificationRe=/\b(?:request|requested|preference|preferred|tentative|pending|subject to|availability|once .{0,24}confirm|will confirm|need to confirm|not confirmed|follow up to confirm)\b/i;
  const unverifiedLines=lines.filter((line:string)=>definitiveBookingRe.test(line)&&!qualificationRe.test(line));
  const unverified=unverifiedLines.length>0;
