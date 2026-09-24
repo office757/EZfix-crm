c0f2de1b742c55ec9f245c1498453d7a9ddd77d3
office757 <office@ezfixgaragedoorsinc.com>
2026-09-24 13:47:29 -0400

    Revoke anonymous access to internal AI and attribution tables

---
+++ supabase/migrations/20260924174706_revoke_anon_internal_ai_and_attribution_tables.sql
@@ -0,0 +1,8 @@
+revoke all on table public.ai_alerts from anon;
+revoke all on table public.ai_daily_briefs from anon;
+revoke all on table public.ai_manager_settings from anon;
+revoke all on table public.ai_recommendations from anon;
+revoke all on table public.ai_usage from anon;
+revoke all on table public.catalog_invoice_link_issues from anon;
+revoke all on table public.lead_attribution from anon;
+revoke all on table public.production_readiness from anon;
